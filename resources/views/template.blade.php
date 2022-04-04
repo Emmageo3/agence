c4af27263ce36ff586a6e6e8e63d1bb0c44194b1
emmageo3 <kanfanyemma22@gmail.com>
2022-04-04 02:16:02 +0000

    navbar

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -8,6 +8,61 @@
     @yield('css')
   </head>
   <body>
+    <header>
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="">
+            <h1>TASNIM IMMO</h1>
+          </a>
+      
+          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+      
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item" href="{{ url('/dashboard') }}">
+              Accueil
+            </a>
+      
+            <a class="navbar-item" href="{{ url('/proprietes') }}">
+              Liste des propriétés
+            </a>
+
+            <a class="navbar-item" href="{{ url('/proprietaires') }}">
+              Liste des propriétaires
+            </a>
+      
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                More
+              </a>
+      
+              <div class="navbar-dropdown">
+                <a class="navbar-item">
+                  About
+                </a>
+                <a class="navbar-item">
+                  Jobs
+                </a>
+                <a class="navbar-item">
+                  Contact
+                </a>
+                <hr class="navbar-divider">
+                <a class="navbar-item">
+                  Report an issue
+                </a>
+              </div>
+            </div>
+          </div>
+      
+          
+        </div>
+      </nav>
+    </header>
     <main class="section">
         <div class="container">
             @yield('content')
