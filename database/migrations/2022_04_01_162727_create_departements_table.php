b2c3256b97bea7b5552ef356a72e4b08d56b76d7
emmageo3 <kanfanyemma22@gmail.com>
2022-04-05 13:30:07 +0000

    departements

---
+++ database/migrations/2022_04_01_162727_create_departements_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::create('departements', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->foreignId('region_id')
+            ->constrained()
+            ->onUpdate('restrict')
+            ->onDelete('restrict'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departements');
+    }
+};
